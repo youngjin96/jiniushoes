34eb5a31e3547bc02d11ab023e2a9007695c8e34
Youngjin <sssk03016@naver.com>
2022-08-17 03:23:41 +0900

    Revising ShoppingBag.js

--- src/Component/ShoppingBag.js
+++ src/Component/ShoppingBag.js
@@ -1,6 +1,10 @@
-import { Button, Grid } from "@mui/material";
 import { useEffect, useState } from "react";
 
+import { Button, Grid } from "@mui/material";
+import { DataGrid } from '@mui/x-data-grid';
+
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import 'rsuite-table/dist/css/rsuite-table.css';
 
 import { collection, getDoc, getDocs, query, where, doc } from "firebase/firestore";
 import { db } from "../Environment/Firebase";
@@ -9,8 +13,16 @@ import { Suspense } from "react";
 
 const uid = sessionStorage.getItem("uid");
 
+
 const ShoppingBag = () => {
     const [shoesDatas, setShoesDatas] = useState([]);
+    const [selectionModel, setSelectionModel] = useState();
+
+    const ImageCell = ({ rowData, dataKey, ...rest }) => (
+        <Cell {...rest}>
+            <img src={rowData[dataKey]} width="100%" height="100%" />
+        </Cell>
+    );
 
     useEffect(() => {
         let arr = [];
@@ -24,6 +36,14 @@ const ShoppingBag = () => {
         })
     }, []);
 
+    const onClickBuy = (e) => {
+        console.log(e.target.id)
+    }
+
+    const onClickRemove = () => {
+        
+    }
+
     if (shoesDatas) {
         return (
             <Grid container columns={{ xs: 12, sm: 12, md: 12 }}>
@@ -32,13 +52,40 @@ const ShoppingBag = () => {
                 </Grid>
                 <Grid item xs={8}>
                     <Grid container columns={{ xs: 12, sm: 12, md: 12 }}>
-                        {shoesDatas.map((item, idx) => {
-                            return (
-                                <Grid key={idx} item xs={2}>
-                                    <img alt={item.name} src={item.img_url} style={{ width: "100%", height: 100 }} />
-                                </Grid>
-                            )
-                        })}
+                        <Grid item xs={12}>
+                            <Table 
+                                data={shoesDatas}
+                                hover={false}
+                                onRowClick={(data) => {
+                                    console.log(data);
+                                }}
+                            >
+                                <Column width={100} align="center" verticalAlign="middle">
+                                    <HeaderCell style={{padding: 0}}>상품</HeaderCell>
+                                    <ImageCell dataKey="img_url" style={{padding: 0, height: "200"}} />
+                                </Column>
+
+                                <Column width={300} align="center" verticalAlign="middle">
+                                    <HeaderCell style={{padding: 0}}>상품명</HeaderCell>
+                                    <Cell dataKey="name" style={{padding: 0}} />
+                                </Column>
+
+                                <Column width={150} align="center" verticalAlign="middle">
+                                    <HeaderCell style={{padding: 0}}>판매가</HeaderCell>
+                                    <Cell dataKey="price" style={{padding: 0}} />
+                                </Column>
+
+                                <Column width={150} align="center" verticalAlign="middle">
+                                    <HeaderCell style={{padding: 0}}>주문관리</HeaderCell>
+                                    <Cell style={{padding: 0}}>
+                                        <Button variant="outlined" onClick={onClickRemove} style={{color: "black", borderColor: "black"}}>
+                                            삭제
+                                        </Button>
+                                    </Cell>
+                                </Column>
+
+                            </Table>
+                        </Grid>
                     </Grid>
                 </Grid>
                 <Grid item xs={2}>
